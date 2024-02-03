f515753e3ad977ae4d726f6a0424ef71bdbb8905
Paul Starodub <amorallex@gmail.com>
2024-02-03 22:18:36 +0200

    update views.py & urls.py & dashboard.html for edit user

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("register/", views.register, name="register"),
+    path("edit/", views.edit, name="edit"),
     path(
         "logout/",
         auth_views.LogoutView.as_view(
